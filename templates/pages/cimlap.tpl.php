f78fc4329e3c854c5df5e02d75ff007cd93c13cd
50M4X <somodisoma97@gmail.com>
2021-05-02 17:42:09 +0200

    Add files via upload

---
+++ templates/pages/cimlap.tpl.php
@@ -0,0 +1,58 @@
+
+<div id="primary">
+		<p>Az Átlátszónet Alapítvány, az atlatszo.hu-val közösen, a kezdetek óta elkötelezett támogatója a társadalmi igazságosság, a demokrácia és a fenntartható fejlődés érvényesülésének, a tényfeltáró újságírás és a civil aktivizmus eszköztárával operálva kívánja elősegíteni a tisztánlátást a közpénzek felhasználásával, a közvagyon sorsával kapcsolatos kérdésekben. Az atlatszo.hu közérdekű bejelentéseket fogad, közérdekű adatokat igényel és publikál, oknyomozó riportokat készít, és ha szükséges, bírósághoz is fordul a közérdekű adatok nyilvánosságának kikényszerítése érdekében.</p>
+		<p>Szervezetünk a KiMitTud közérdekű adatigénylő portál működtetésében és népszerűsítésében is aktív szerepet vállal, specifikus workshopokat tart a helyi civilek által működtetett független hírportálok, közéleti blogok, aktivista Facebook-közösségek, specialista civil szervezetek számára, melyek célja a közösségi watchdog módszertan átadása, ezen keresztül segítve az állampolgárok informált döntéshozatalhoz való jogának érvényesülését, valamint a közérdekű adatigénylés mint a részvételi demokrácia egyik hatékony eszközének megismerését és elterjesztését.</p>
+<p>&nbsp;</p>
+<ul>
+<li>alapítás éve: 2013</li>
+<li><a title="Alapító okirat" style="text-decoration:none; color:#16A085;" href="https://www.dropbox.com/s/fliteewofc6yntx/alapito-okirat-egys%C3%A9ge_2017-08-04.pdf?dl=0" target="_blank">alapító okirat</a></li>
+<li>a kuratórium elnöke: dr. Bodoky Tamás Richárd</li>
+<li>kuratóriumi tagok: dr. Jóri András, dr. Polyák Gábor György</li>
+<li>székhely:&nbsp;1084 Budapest, Déri Miksa utca 10. 4. em. 9. ajtó</li>
+<li>nyilvántartási szám:&nbsp;01-01-0011774</li>
+<li>adószám:&nbsp;18516641-1-42</li>
+<li>bankszámlaszám:&nbsp;12011265-01425189-00100001</li>
+<li>SWIFT: UBRTHUHB</li>
+<li>IBAN: HU78 1201 1265 0142 5189 0030 0005</li>
+</ul>
+<p>&nbsp;</p>
+
+<div id="videocontainer">
+<video width="520" height="292" controls>
+  <source src="./videos/test.mp4" type="video/mp4">
+</video>
+<iframe width="520" height="292" frameBorder="0"
+src="https://www.youtube.com/embed/lqZehFAwoTM">
+</iframe>
+</div>
+</div>
+
+	<div id="secondary">
+<aside id="search-2" class="widget clearfix widget_search"><form role="search" method="get" class="search-form form-inline" action="https://atlatszonet.hu/">
+<div class="form-group">
+	<label>
+		<span ></span>
+		<input type="search" class="searchfield" placeholder="Search …" value="" name="s">
+	</label>
+	<input type="submit" class="button" value="Search">
+</div><!-- .form-group -->
+</form>
+</aside>
+<div ><aside id="text-3" class="widget clearfix widget_text"><h2 class="widget-title">#Azenadombol</h2><div class="textwidget"><a href="http://atlatszo.hu/azenadombol/"><img src="./images/banner.gif"></a></div><a href="http://atlatszo.hu/azenadombol/"></div>
+		<div >
+		</a></aside><aside id="rss-2">	
+<div id="rss">	<h2 class="widget-title2"><a class="rsswidget" href="http://atlatszo.hu/feed"><img style="border:0; padding-top:10px" width="14" height="14" src="./images/rss.png" alt="RSS"></a><a class="rsswidget"  href="https://atlatszo.hu/">atlatszo.hu</a></h2></div>		
+		<ul class="jobboldal">
+	
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/27/panoramas-vizparti-telkek-elovasarlasi-jogarol-mondott-le-az-onkormanyzat-balatonakarattyan/">Panorámás, vízparti telkek elővásárlási jogáról mondott le az önkormányzat Balatonakarattyán</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/26/400-helyett-4100-hordo-radioaktiv-hulladekot-utaztatnanak-at-az-orszagon-bataapatiba/">400 helyett 4100 hordó radioaktív hulladékot utaztatnának át az országon Bátaapátiba</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/22/letarolnak-tihanyban-a-balaton-partot-egy-medences-luxusszallo-miatt/">Letarolnák Tihanyban a Balaton-partot egy medencés luxusszálló miatt</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/20/meszaros-lorinc-kemenyitogyara-andrej-babis-etolajos-cege-es-a-whb-is-kapott-kedvezmenyes-covid-hitelt-az-eximbanktol/">Mészáros Lőrinc keményítőgyára, Andrej Babis étolajos cége és a WHB is kapott kedvezményes COVID-hitelt az Eximbanktól</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/19/ocsai-es-inarcsi-gazdak-lehetnek-a-vesztesei-magyarorszag-legnagyobb-napelemparkjanak/">Ócsai és inárcsi gazdák lehetnek a vesztesei Magyarország legnagyobb napelemparkjának</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/15/aki-elutasit-egy-vakcinat-elutasitja-az-eletet-orvosok-asszisztensek-az-oltasi-kampanyrol/">„Aki elutasít egy vakcinát, elutasítja az életet” – orvosok, asszisztensek az oltási kampányról</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/14/ez-a-kormany-is-nekifut-a-motogp-projektnek-de-mar-a-foldek-kisajatitasa-sem-megy-atlathatoan/">Ez a kormány is nekifut a MotoGP-projektnek, de már a földek kisajátítása sem megy átláthatóan</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/13/andrassy-emlekmuzeum-helyett-demeter-szilard-ugynoksege-koltozhet-a-bem-rakparti-palotaba/">Andrássy Emlékmúzeum helyett Demeter Szilárd ügynöksége költözhet a Bem rakparti palotába</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/12/birsag-tilalom-kover-laszlot-majmolja-a-kerepesi-polgarmester-onkenyesen-atirta-az-szmsz-t/">Bírság, tilalom: Kövér Lászlót majmolja a kerepesi polgármester, önkényesen átírta az SZMSZ-t</a></li>
+		<li><a class="rsswidget" href="https://atlatszo.hu/2021/04/09/egy-uj-tanulmany-szerint-elmismasoltak-a-paksi-foldrengesbiztonsagrol-szolo-foldtani-kutatast/">Egy új tanulmány szerint elmismásolták a paksi földrengésbiztonságról szóló földtani kutatást</a></li></ul></aside>	</div><!-- #secondary -->
+	</div>
+		
